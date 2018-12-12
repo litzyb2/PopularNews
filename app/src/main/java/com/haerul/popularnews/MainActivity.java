eb18e728f3d4ca980820d182b385d0544391b432
litzybucio <litzyb2@illinois.edu>
2018-12-12 04:40:55 -0600

    working lmao

--- app/src/main/java/com/haerul/popularnews/MainActivity.java
+++ app/src/main/java/com/haerul/popularnews/MainActivity.java
@@ -39,7 +39,7 @@ import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity implements  SwipeRefreshLayout.OnRefreshListener{
 
-    public static final String API_KEY = "your secret api key";
+    public static final String API_KEY = "176fc25bae4e4fdd91325ea1b5e27f2c";
     private RecyclerView recyclerView;
     private RecyclerView.LayoutManager layoutManager;
     private List<Article> articles = new ArrayList<>();
